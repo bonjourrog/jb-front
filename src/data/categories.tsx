9a8ab97cb300e07198d74c65792f5c3fe24366c5
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-04-07 16:57:07 -0700

    feat: create category data mock

---
+++ src/data/categories.tsx
@@ -0,0 +1,11 @@
+import { PiHeadsetFill } from "react-icons/pi";
+import { Category } from "../types/categories";
+
+export const CATEGORIES_DATA_MOCK:Category[] = [
+    {
+        id:1,
+        icon:<PiHeadsetFill className='categories__logo'/>,
+        title: 'Atención al cliente',
+        description:"Cajero, representante de ventas, servicio..."
+    }
+] 
