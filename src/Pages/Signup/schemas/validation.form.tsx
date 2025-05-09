8a1d1ce0b4fbfbdc6a4defc5c955c3ac6c8028d2
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-08 20:35:03 -0700

    feat: create userSchema validation for user register

---
+++ src/Pages/Signup/schemas/validation.form.tsx
@@ -0,0 +1,12 @@
+import { z } from "zod";
+export const userSchema = z.object({
+    name: z.string().min(2, "Nombre muy corto"),
+    last_name: z.string().min(2, "Apellido muy corto"),
+    email: z.string().email("Correo inválido"),
+    phone: z.string().min(10, "Teléfono inválido"),
+    password: z.string().min(6, "La contraseña es muy corta"),
+    location: z.string().min(2, "Ubicación requerida"),
+    photo: z
+        .instanceof(FileList)
+        .refine((files) => files.length > 0, "Se requiere una foto"),
+});
