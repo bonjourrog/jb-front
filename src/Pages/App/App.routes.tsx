5c81709b36fde618ac5d1e745a17d913e014b3f3
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-08 20:39:43 -0700

    feat: implement signup route

--- src/Pages/App/App.routes.tsx
+++ src/Pages/App/App.routes.tsx
@@ -1,9 +1,11 @@
 import { useRoutes } from "react-router"
 import Home from "../Home";
+import Signup from "../Signup";
 
 const AppRoutes = ()=>{
     let routes =useRoutes([
         {path:'/', element:<Home/>},
+        {path:'/signup', element:<Signup/>},
         {path:'*', element:<p>404 Not Found</p>}
     ])
     return routes;
