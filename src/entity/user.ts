0c23814b97d4a4433f8b8a6cb9658f0ffb601a2e
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-08 20:30:22 -0700

    feat: create user entity

---
+++ src/entity/user.ts
@@ -0,0 +1,31 @@
+export interface User{
+    _id:string;
+	name:string;
+	last_name:string;
+	account:Account;
+	role:string;
+	company:Company;
+	created_at:Date;
+	updated_at:Date;
+}
+interface Account{
+    email:string;
+	password:string;
+	phone:string;
+	banned:boolean;
+}
+interface Company{
+    name:string;
+	logo:string;
+	address:Address;
+}
+interface Address{
+    first_street:string;
+	second_street:string;
+	neighborhood:string;
+	location:Location;
+}
+interface Location{
+    type:string; // always "Point"
+	coordinates:number[];
+}
