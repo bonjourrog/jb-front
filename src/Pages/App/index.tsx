d57655bf0af12ee13855a98c4a148b32de10a3a0
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-11 13:47:05 -0700

    chore: rehydrate auth token on app load

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -3,8 +3,29 @@ import { BrowserRouter } from 'react-router'
 import { ToastContainer } from 'react-toastify';
 import AppRoutes from './App.routes'
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect } from 'react';
+import { jwtDecode } from 'jwt-decode';
+import { useAuthStore } from '../../stores/authStore';
 
 function App() {
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    console.log(token);
+    
+    if(token){
+      try {
+        const decoded:any = jwtDecode(token)
+        const isValid = decoded.exp*1000 > Date.now();
+        if(isValid){
+          useAuthStore.getState().login(token);
+        }else{
+          useAuthStore.getState().logout()
+        }
+      } catch (error) {
+        useAuthStore.getState().logout()
+      }
+    }
+  },[])
   return (
     <BrowserRouter>
       <ToastContainer />
