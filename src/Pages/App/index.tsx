7fa38cb4f9027ea7487084d22d9e4e122643e6af
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-10 19:47:55 -0700

    feat: implement toastContainer component in app component

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,10 +1,13 @@
-import { BrowserRouter } from 'react-router'
 import './App.css'
+import { BrowserRouter } from 'react-router'
+import { ToastContainer } from 'react-toastify';
 import AppRoutes from './App.routes'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <AppRoutes/>
     </BrowserRouter>
   )
