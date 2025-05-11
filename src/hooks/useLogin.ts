b706fa8341b9afe5489f91ea1f8f88a7f931e144
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-11 13:19:56 -0700

    feat: create custome hook useLogin

---
+++ src/hooks/useLogin.ts
@@ -0,0 +1,31 @@
+import { useState } from "react"
+import { useAuthStore } from "../stores/authStore";
+import { useNavigate } from "react-router";
+import { signin } from "../service/auth";
+import { toast } from "react-toastify";
+
+export const useLogin = ()=>{
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const login = useAuthStore(s=>s.login);
+    const navigate = useNavigate();
+
+    const handleLogin = async(credentials:{email:string, password:string})=>{
+        try {
+            setIsLoading(true)
+            const {data:token} = await signin(credentials)
+            login(token)
+            toast.success('Usuario creado con éxito', {
+                position: 'top-right',
+                autoClose: 3000,
+                hideProgressBar: false,
+                theme: 'light',
+            });
+            navigate('/')
+        } catch (error:any) {
+            toast.error(error?.response?.data?.error || 'Error al iniciar sesión');
+        }finally{
+            setIsLoading(false);
+        }
+    }
+    return {handleLogin, isLoading}
+}
