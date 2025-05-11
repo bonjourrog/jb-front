f07de2c59a9f6fc216f1ca60e068757983697f9e
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-11 13:18:22 -0700

    feat: create signin function in auth service

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -4,3 +4,6 @@ import api from "./api"
 export const signup = async(user:Partial<User>)=>{
     return await api.post('auth/signup', user);
 }
+export const signin = async(credentials:{email:string, password:string})=>{
+    return (await api.post('auth/signin', credentials)).data;
+}
