2f3a2dc56dc8ef6b83621548b33a465c9d430f8d
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-11 12:17:06 -0700

    feat: create type LoginData

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,6 @@
 import { z } from "zod";
 import { userSchema } from "../Pages/Signup/schemas/validation.form";
+import { loginSchema } from "../Pages/Signin/schemas/login";
 
 export type RegisterData = z.infer<typeof userSchema>;
+export type LoginData = z.infer<typeof loginSchema>
