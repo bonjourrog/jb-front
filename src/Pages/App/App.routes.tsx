0900a89607e956ff188a63908332aa60e4a46fae
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-11 19:17:19 -0700

    fix: set Results component for / and create /landing for Home component

--- src/Pages/App/App.routes.tsx
+++ src/Pages/App/App.routes.tsx
@@ -2,10 +2,12 @@ import { useRoutes } from "react-router"
 import Home from "../Home";
 import Signup from "../Signup";
 import Signin from "../Signin";
+import Results from "../Results";
 
 const AppRoutes = ()=>{
     let routes =useRoutes([
-        {path:'/', element:<Home/>},
+        {path:'/', element:<Results/>},
+        {path:'/landing', element:<Home/>},
         {path:'/signup', element:<Signup/>},
         {path:'/login', element:<Signin/>},
         {path:'*', element:<p>404 Not Found</p>}
