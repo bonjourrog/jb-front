7bffe6a2645e6872ba2d30c64bfdb362d09de990
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-17 16:48:33 -0700

    chore: create job entity '

---
+++ src/entity/job.ts
@@ -0,0 +1,21 @@
+export interface Job {
+    _id: string;
+    title: string;
+    short_description: string;
+    description: string;
+    salary: number;
+    benefits: string[];
+    location: Location;
+    industry: string;
+    schedule: string;
+    contract_type: string;
+    is_formal_job: boolean;
+    published: boolean;
+    company_id: string;
+    created_at: Date;
+    updated_at: Date;
+}
+export interface Location {
+    type: string;               // always "Point"
+    coordinates: number[] // [longitude, latitude]
+}
