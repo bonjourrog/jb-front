5046f4e39996d5c84da9c1d8c1c0d46472a33891
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-25 16:30:28 -0700

    style: fix job content height

--- src/Pages/Results/Components/Jobs/index.tsx
+++ src/Pages/Results/Components/Jobs/index.tsx
@@ -36,7 +36,7 @@ const Jobs = () => {
                     const color = colorGenerator()
                     return (
                     <li key={job._id} className='job' onClick={() => setShowDetails(!showDetails)}>
-                        <div style={{background:color}} className={`flex items-center justify-center w-16 h-16 max-h-16 rounded-lg overflow-hidden`}>
+                        <div style={{background:color}} className={`flex items-center justify-center w-16 h-16 max-h-16 max-w-16 rounded-lg overflow-hidden shrink-0`}>
                             {
                                 job.company_logo?<img className='h-full object-cover' src={job.company_logo} alt="" />:<div className='flex items-center justify-center min-w-12 min-h-12 font-bold text-zinc-700 bg-white rounded-full'>jb</div>
                             }
