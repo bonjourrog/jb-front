e930841d7d76f28b6688da54a87a1cba00ec08ed
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-05-31 16:56:49 -0700

    style: change dashboard color style

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,13 +1,21 @@
 import './Dashboard.css';
 import { useAuthStore } from "../../stores/authStore";
+import { BiBriefcase } from 'react-icons/bi';
 
 const Dashboard = ()=>{
     const {} = useAuthStore()
     return <main className="dashboard">
-        <aside className='dashboard__sidebar'></aside>
+        <aside className='dashboard__sidebar'>
+            <ul>
+                <li>
+                    <BiBriefcase/>
+                    Empleos
+                </li>
+            </ul>
+        </aside>
         <section className='dashboard__content'>
             <header>
-                <strong className='text-zinc-700 font-bold'>Welcome</strong>
+                <strong className='text-[#1d1c22] font-bold'>Welcome</strong>
             </header>
             {/* some cards with ingo */}
             <section className='dashboard__cards'>
