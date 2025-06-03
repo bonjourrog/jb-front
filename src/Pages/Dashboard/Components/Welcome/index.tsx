e86fef4bea66604982fbb4f36fa2c6b35a2249b7
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-06-02 19:31:13 -0700

    feat: create Welcome component
    this represent the main page of the dashboard

---
+++ src/Pages/Dashboard/Components/Welcome/index.tsx
@@ -0,0 +1,25 @@
+import './Welcome.css';
+const Welcome = () => {
+    return <>
+        {/* some cards with ingo */}
+        <section className='welcome__cards'>
+            <ul>
+                <li>h</li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </section>
+        {/* some charts */}
+        <section className='welcome__charts'>
+            <div></div>
+        </section>
+        {/* a list of user that are appliying */}
+        <section className='welcome__recent-applicants'>
+            <div>
+
+            </div>
+        </section>
+    </>
+}
+export default Welcome;
