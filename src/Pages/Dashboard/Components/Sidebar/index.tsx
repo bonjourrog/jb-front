d850e70b6f10e0bbb2ae3462cef6ea6273d0b123
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-06-02 19:49:58 -0700

    style: implement an active class to style dashboard sidebar links depending the route

--- src/Pages/Dashboard/Components/Sidebar/index.tsx
+++ src/Pages/Dashboard/Components/Sidebar/index.tsx
@@ -1,14 +1,24 @@
+import { Link, useLocation } from 'react-router';
 import './Sidebar.css';
-import { BiBriefcase } from "react-icons/bi"
+import { BiBriefcase, BiHomeAlt } from "react-icons/bi"
 
-const Sidebar = ()=>{
+const Sidebar = () => {
+    const location = useLocation();
     return <aside className='dashboard-sidebar'>
-            <ul>
-                <li>
-                    <BiBriefcase/>
+        <ul>
+            <li className={ location.pathname === '/dashboard'?'active':''}>
+                <Link to={'/dashboard'} className='dashboard-link'>
+                    <BiHomeAlt />
+                    Home
+                </Link>
+            </li>
+            <li className={ location.pathname === '/dashboard/jobs'?'active':''}>
+                <Link to={'/jobs'} className='dashboard-link'>
+                    <BiBriefcase />
                     Empleos
-                </li>
-            </ul>
-        </aside>
+                </Link>
+            </li>
+        </ul>
+    </aside>
 }
 export default Sidebar;
