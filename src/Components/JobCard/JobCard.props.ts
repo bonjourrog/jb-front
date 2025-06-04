488f1140839b50a4781e6d3255eb052324ab51c2
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-06-04 16:11:27 -0700

    chore: add className and icon properties into JobCardProperties

--- src/Components/JobCard/JobCard.props.ts
+++ src/Components/JobCard/JobCard.props.ts
@@ -1,8 +1,10 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, ReactNode, SetStateAction } from "react";
 import { Job } from "../../entity/job";
 
 export interface JobCardProps {
     job: Job;
     color: string;
-    setShowDetails: Dispatch<SetStateAction<boolean>>;
+    setShowDetails?: Dispatch<SetStateAction<boolean>>;
+    className?: string;
+    icon?: ReactNode;
 }
