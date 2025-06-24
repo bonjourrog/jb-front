9de88b28ec45cdba27e6990ff47cda4e5e9174d8
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-06-24 15:17:23 -0700

    feat: add the function createJob into job service

--- src/service/job.ts
+++ src/service/job.ts
@@ -1,6 +1,11 @@
 import { Filter } from "../entity/filter";
+import { Job } from "../entity/job";
 import api from "./api";
 
 export const getJobs = async(filters:Filter)=>{
     return (await api.get('job', {params:{...filters}})).data;
+}
+
+export const createJob = async(job:Job, token:string)=>{
+    return (await api.post('job', job, {headers:{'Authorization':token}})).data
 }
