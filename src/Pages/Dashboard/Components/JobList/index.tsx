4eb1b0af5ac86d060ad4af4e309c80f59953d2d5
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-06-24 19:16:42 -0700

    feat: add description and short description columns into job list table

--- src/Pages/Dashboard/Components/JobList/index.tsx
+++ src/Pages/Dashboard/Components/JobList/index.tsx
@@ -4,10 +4,10 @@ import { jwtDecode as jwt_decode } from 'jwt-decode';
 import { useAuthStore } from "../../../../stores/authStore";
 import { Filter } from "../../../../entity/filter";
 import { useJobs } from "../../../../hooks/useJobs";
-import { FormControlLabel, FormGroup, styled, Switch, SwitchProps } from '@mui/material';
-import { FiEye } from 'react-icons/fi';
+import { FormControlLabel, FormGroup, styled, Switch, SwitchProps, Tooltip } from '@mui/material';
 import { MdAdd } from 'react-icons/md';
 import JobListForm from '../../../../Components/NewJobForm';
+import { MoreVert } from '@mui/icons-material';
 
 const JobList = () => {
     const { token } = useAuthStore();
@@ -26,15 +26,15 @@ const JobList = () => {
     const IOSSwitch = styled((props: SwitchProps) => (
         <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
     ))(({ theme }) => ({
-        width: 42,
-        height: 26,
+        width: '100%',
+        height: 30,
         padding: 0,
         '& .MuiSwitch-switchBase': {
             padding: 0,
-            margin: 2,
+            margin: 3,
             transitionDuration: '300ms',
             '&.Mui-checked': {
-                transform: 'translateX(16px)',
+                transform: 'translateX(70px)',
                 color: '#fff',
                 '& + .MuiSwitch-track': {
                     backgroundColor: '#65C466',
@@ -67,8 +67,8 @@ const JobList = () => {
         },
         '& .MuiSwitch-thumb': {
             boxSizing: 'border-box',
-            width: 22,
-            height: 22,
+            width: 24,
+            height: 24,
         },
         '& .MuiSwitch-track': {
             borderRadius: 26 / 2,
@@ -84,55 +84,67 @@ const JobList = () => {
     }));
 
     return <section className='p-10'>
-        <div className={`absolute w-full h-full ${showForm?'top-1/2':'-top-full'} left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-zinc-700/35`}>
-            <JobListForm setShowForm={setShowForm}/>
+        <div className={`absolute w-full h-full ${showForm ? 'top-1/2' : '-top-full'} left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-zinc-700/35`}>
+            <JobListForm setShowForm={setShowForm} />
         </div>
         <div className='overflow-x-auto p-1 w-auto'>
             <table>
-            <thead>
-                <tr>
-                    <th>Puesto</th>
-                    <th>Salario</th>
-                    <th>Beneficios</th>
-                    <th>Publicado</th>
-                    <th>Ver detalles</th>
-                </tr>
-            </thead>
-            <tbody>
-                {jobs.map(job => (
+                <thead>
                     <tr>
-                        <td className='editable'>
-                            <strong>{job.title}</strong>
-                        </td>
-                        <td className='editable'>
-                            <p className='text-zinc-500'>
-                                {Math.trunc(job.salary).toLocaleString('en-US', {style:'currency', currency:'usd', minimumFractionDigits: 0})}
-                            </p>
-                        </td>
-                        <td className='editable'>
-                            <div className='flex gap-2'>
-                                {job.benefits.map(benefit=>(
-                                    <p className='px-2 py-1 text-xs text-emerald-600 bg-emerald-200 rounded-sm'>{benefit}</p>
-                                ))}
-                            </div>
-                        </td>
-                        <td className='editable'>
-                            <FormGroup>
-                                <FormControlLabel
-                                    control={<IOSSwitch sx={{ m: 1 }} checked={job.published}/>}
-                                    label=""
-                                />
-                                {job.published}
-                            </FormGroup>
-                        </td>
-                        <td className='editable'><FiEye/></td>
+                        <th></th>
+                        <th>Puesto</th>
+                        <th>Salario</th>
+                        <th>Beneficios</th>
+                        <th>Descriopcion</th>
+                        <th>Descripcion corta</th>
+                        <th>Publicado</th>
                     </tr>
-                ))}
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    {jobs.map(job => (
+                        <tr>
+                            <td className='hover:bg-gray-100'>
+                                <div className='flex gap-2 justify-center items-start'>
+                                    <MoreVert className='text-gray-400' />
+                                </div>
+                            </td>
+                            <td className='editable'>
+                                <strong>{job.title}</strong>
+                            </td>
+                            <td className='editable'>
+                                <p className='text-zinc-500'>
+                                    {Math.trunc(job.salary).toLocaleString('en-US', { style: 'currency', currency: 'usd', minimumFractionDigits: 0 })}
+                                </p>
+                            </td>
+                            <td className='editable'>
+                                <div className='flex gap-2'>
+                                    {job.benefits.map(benefit => (
+                                        <p className='px-2 py-1 text-xs text-emerald-600 bg-emerald-200 rounded-sm'>{benefit}</p>
+                                    ))}
+                                </div>
+                            </td>
+                            <td className='editable max-w-96 text-gray-400 text-sm font-bold'>
+                                {job.description.length > 30 ? `${job.description.substring(0, 30)}...` : job.description}
+                            </td>
+                            <td className='editable max-w-96 text-zinc-400 text-sm font-bold'>
+                                {job.short_description.length > 30 ? `${job.short_description.substring(0, 30)}...` : job.short_description}
+                            </td>
+                            <td className='editable'>
+                                <FormGroup>
+                                    <FormControlLabel
+                                        control={<IOSSwitch sx={{ m: 1 }} checked={job.published} />}
+                                        label=""
+                                    />
+                                    {job.published}
+                                </FormGroup>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-        <div className='new-job-btn' onClick={()=>setShowForm(true)}>
-            <MdAdd/>New job
+        <div className='new-job-btn' onClick={() => setShowForm(true)}>
+            <MdAdd />New job
         </div>
         {/* <ul className='grid grid-cols-3 gap-4 p-4'>
             {jobs.map(job => (
