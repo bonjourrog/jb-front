3274d89cc5f8d2bf557f0594dd6ddfb9296dbdfa
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-07-03 20:51:39 -0700

    feat: add jobs, setJobs and updateJobs new properties into useJobState

--- src/stores/jobStore.ts
+++ src/stores/jobStore.ts
@@ -2,13 +2,27 @@ import { create } from "zustand";
 import { Job } from "../entity/job";
 
 interface JobState{
+    jobs:Job[];
     job: Job;
     setJob:(job: Job)=>void;
+    setJobs:(jobs:Job[])=>void;
+    updateJobs:(updatedJob:Job)=>void;
 }
 
 export const useJobStore = create<JobState>(set=>({
+    jobs:[],
     job:{} as Job,
     setJob:(job:Job)=>{
         set({job})
+    },
+    setJobs:(jobs:Job[])=>{
+        set({jobs})
+    },
+    updateJobs:(updatedJob:Job)=>{
+        set((state)=>({
+            jobs: state.jobs.map(job=>
+                job._id===updatedJob._id ?{...job, ...updatedJob}:job
+            )
+        }))
     }
 }))
