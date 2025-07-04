901dbe5b75842f2880dea0092d2056cb8dc08ebb
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-07-03 20:52:32 -0700

    feat: add jobData into newJobForm props interface

--- src/Components/NewJobForm/newJobForm.props.ts
+++ src/Components/NewJobForm/newJobForm.props.ts
@@ -1,5 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
+import { Job } from "../../entity/job";
 
 export interface NewJobFormProps {
+    jobData: Job | null;
     setShowForm: Dispatch<SetStateAction<boolean>>;
 }
