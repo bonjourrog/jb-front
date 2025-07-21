ecdd19ee66db396a222a67ce417341b8f79f66b9
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-07-20 21:48:25 -0700

    chore: delete unecessary console log in job service

--- src/service/job.ts
+++ src/service/job.ts
@@ -10,8 +10,6 @@ export const newJob = async(job:Job, token:string)=>{
     return (await api.post('job', job, {headers:{'Authorization':token}})).data
 }
 
-export const updateJob = async(job:Job, token:string)=>{
-    console.log(job);
-    
+export const updateJob = async(job:Job, token:string)=>{   
     return (await api.put('job', job, {headers:{'Authorization':token}})).data
 }
