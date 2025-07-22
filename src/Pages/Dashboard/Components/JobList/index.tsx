df245fa501e10f61d54913483580da85c2dfee7f
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-07-21 19:28:08 -0700

    feat: create SwitchComponent

--- src/Pages/Dashboard/Components/JobList/index.tsx
+++ src/Pages/Dashboard/Components/JobList/index.tsx
@@ -4,12 +4,13 @@ import { jwtDecode as jwt_decode } from 'jwt-decode';
 import { useAuthStore } from "../../../../stores/authStore";
 import { Filter } from "../../../../entity/filter";
 import { useJobs } from "../../../../hooks/useJobs";
-import { FormControlLabel, FormGroup, Menu, MenuItem, styled, Switch, SwitchProps} from '@mui/material';
+import { FormControlLabel, FormGroup, Menu, MenuItem} from '@mui/material';
 import { MdAdd } from 'react-icons/md';
 import JobListForm from '../../../../Components/NewJobForm';
 import { Delete, Edit, MoreVert } from '@mui/icons-material';
 import { Job } from '../../../../entity/job';
 import { useJobStore } from '../../../../stores/jobStore';
+import SwitchComponent from './Components/SwitchComponent';
 
 const JobList = () => {
     const { token } = useAuthStore();
@@ -40,65 +41,7 @@ const JobList = () => {
         setJobs(newJobs);
     }
 
-    const IOSSwitch = styled((props: SwitchProps) => (
-        <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
-    ))(({ theme }) => ({
-        width: '100%',
-        height: 30,
-        padding: 0,
-        '& .MuiSwitch-switchBase': {
-            padding: 0,
-            margin: 3,
-            transitionDuration: '300ms',
-            '&.Mui-checked': {
-                transform: 'translateX(70px)',
-                color: '#fff',
-                '& + .MuiSwitch-track': {
-                    backgroundColor: '#65C466',
-                    opacity: 1,
-                    border: 0,
-                    ...theme.applyStyles('dark', {
-                        backgroundColor: '#2ECA45',
-                    }),
-                },
-                '&.Mui-disabled + .MuiSwitch-track': {
-                    opacity: 0.5,
-                },
-            },
-            '&.Mui-focusVisible .MuiSwitch-thumb': {
-                color: '#33cf4d',
-                border: '6px solid #fff',
-            },
-            '&.Mui-disabled .MuiSwitch-thumb': {
-                color: theme.palette.grey[100],
-                ...theme.applyStyles('dark', {
-                    color: theme.palette.grey[600],
-                }),
-            },
-            '&.Mui-disabled + .MuiSwitch-track': {
-                opacity: 0.7,
-                ...theme.applyStyles('dark', {
-                    opacity: 0.3,
-                }),
-            },
-        },
-        '& .MuiSwitch-thumb': {
-            boxSizing: 'border-box',
-            width: 24,
-            height: 24,
-        },
-        '& .MuiSwitch-track': {
-            borderRadius: 26 / 2,
-            backgroundColor: '#E9E9EA',
-            opacity: 1,
-            transition: theme.transitions.create(['background-color'], {
-                duration: 500,
-            }),
-            ...theme.applyStyles('dark', {
-                backgroundColor: '#39393D',
-            }),
-        },
-    }));
+    
     return <section className='p-10'>
         <div className={`absolute w-full h-full ${showForm ? 'top-1/2' : '-top-full'} left-1/2 -translate-x-1/2 -translate-y-1/2 z-20 bg-zinc-700/35`}>
             <JobListForm setShowForm={setShowForm} jobData={jobSelected}/>
@@ -155,7 +98,7 @@ const JobList = () => {
                             <td className='editable'>
                                 <FormGroup>
                                     <FormControlLabel
-                                        control={<IOSSwitch sx={{ m: 1 }} checked={job.published} />}
+                                        control={<SwitchComponent sx={{ m: 1 }} checked={job.published} />}
                                         label=""
                                     />
                                     {job.published}
