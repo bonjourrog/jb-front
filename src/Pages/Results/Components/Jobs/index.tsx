33322b2a9c8bf5517240e8925a20cbd287ed97eb
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-08-20 15:40:11 -0700

    feat: set user_id in filters if user is logged in

--- src/Pages/Results/Components/Jobs/index.tsx
+++ src/Pages/Results/Components/Jobs/index.tsx
@@ -1,10 +1,12 @@
 import './Jobs.css';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Filter } from "../../../../entity/filter";
 import { useJobs } from "../../../../hooks/useJobs";
 import { useJobStore } from '../../../../stores/jobStore';
 import Details from '../Details';
 import JobCard from '../../../../Components/JobCard';
+import { useAuthStore } from '../../../../stores/authStore';
+import {jwtDecode as jwt_decode} from 'jwt-decode';
 
 const Jobs = () => {
     const [showDetails, setShowDetails] = useState<boolean>(false);
@@ -16,13 +18,29 @@ const Jobs = () => {
         schedule: '',
         search: ''
     });
+    const {token} = useAuthStore();
+    
+
     const colorGenerator = (): string=> {
         const hue = Math.floor(Math.random() * 360);         // 0–360
         const saturation = Math.floor(Math.random() * 20) + 60; // 60–80%
         const lightness = Math.floor(Math.random() * 15) + 60;  // 60–75%
         return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
     }
-    const {} = useJobs(filters);
+
+    useEffect(()=>{
+        
+        if(token){
+            const decoed:any = jwt_decode(token);
+            setFilters(prev => ({
+                ...prev,
+                user_id: decoed.userId
+            }))
+        }
+    }, [token])
+    
+    useJobs(filters);
+
     return <section className='jobs'>
         <strong>Recientes</strong>
         <Details job={job} setShowDetails={setShowDetails} showDetails={showDetails}/>
