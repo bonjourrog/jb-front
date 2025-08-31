59f9cc20f73d6454c6a8a0b7c60e88fcbd6e31d2
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-08-30 19:33:05 -0700

    feat: create Applied component

---
+++ src/Pages/Applied/index.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react"
+import { useUserApplication } from "../../hooks/useUserApplication"
+import { Application } from "../../entity/application"
+import AppsTable from "./Components/AppsTable"
+
+const Applied = () => {
+    const { getApplication } = useUserApplication()
+    const [apps, setApps] = useState<Application[]>([])
+    const handleGetApplications = async () => {
+        const { data, error, status } = await getApplication()
+        if (status === 'success' && data != null) {
+            const { applications } = data
+            const _applications: Application[] = applications.map((elem: any) => ({ ...elem.application, job: elem.company }))
+            setApps(_applications)
+            console.log(_applications);
+        } else {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        handleGetApplications()
+    }, [])
+    return <section className="flex flex-col gap-10 w-full h-full bg-slate-100 rounded-lg p-10">
+        <section>
+            <nav>
+                <ul className="flex gap-10">
+                    <li className="border-b border-blue-400 cursor-pointer">postulaciones</li>
+                    <li className="border-b border-green-400 cursor-pointer">Guardados</li>
+                </ul>
+            </nav>
+        </section>
+        <div className="h-full max-h-full">
+            {
+                apps.length > 0 ?
+                    <AppsTable apps={apps}/>
+                    : <p>LAs vacantes que apliques apareceran aqui</p>
+            }
+        </div>
+
+    </section>
+}
+export default Applied
