7f1dafc7b9400efd8a659c5a93e0782a94aff58f
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-09-07 16:43:14 -0700

    feat(entity): change the Application entity structure

--- src/entity/application.ts
+++ src/entity/application.ts
@@ -1,12 +1,12 @@
 import { Job } from "./job";
 
-export interface Application{
-    _id:string;
+interface AppFields{
+	_id:string;
 	user_id: string;
 	company_id: string;
 	job_id: string;
 	status: string;
 	applied_at: Date;
-    job: Job;
 }
+export type Application = AppFields & Job;
 
