553ccf2e27e0e10df22d8e8241cb73d3f7ec94e6
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-04 07:57:21 -0700

    feat: implement search by filters

--- src/Pages/Results/Components/Filters/index.tsx
+++ src/Pages/Results/Components/Filters/index.tsx
@@ -1,28 +1,45 @@
-import { Checkbox, FormControlLabel, FormGroup, Radio, RadioGroup } from '@mui/material';
+import { FormControlLabel, Radio, RadioGroup } from '@mui/material';
 import './Filters.css';
 import { ChangeEvent, useState } from 'react';
+import { useJobStore } from '../../../../stores/jobStore';
 const Filters = () => {
-    const [salary, setSalary] = useState<{min:string, max:string}>({
-        min:'',
-        max:''
+    const [salary, setSalary] = useState<{ min: string, max: string }>({
+        min: '',
+        max: ''
     })
-    const handleSalarychange = (event:ChangeEvent<HTMLInputElement>)=>{
-        setSalary({...salary,[event.target.name]:event.target.value});
+    const filters = useJobStore(state => state.filters);
+    const setFilters = useJobStore(state => state.setFilters);
+
+    const handleSalarychange = (event: ChangeEvent<HTMLInputElement>) => {
+        setSalary({ ...salary, [event.target.name]: event.target.value });
+    }
+    const handleContractChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setFilters({
+            ...filters,
+            contract: event.target.value === 'limpiar' ? '' : event.target.value
+        });
+    }
+    const handleScheduleChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setFilters({
+            ...filters,
+            schedule: event.target.value === 'limpiar' ? '' : event.target.value
+        });
     }
     return <ul className='filters'>
         <li>
             <strong>Tipo de empleo</strong>
             <RadioGroup
+            onChange={handleContractChange}
                 sx={{
                     '& .MuiSvgIcon-root': { color: '#656565' },
                     color: '#494949', pl: 2, fontSize: '.1em',
                 }}>
-                <FormControlLabel value="medio tiempo" control={<Radio sx={{p:.5}} size='small' />} label="Medio tiempo" />
-                <FormControlLabel value="tiempo completo" control={<Radio sx={{p:.5}} size='small' />} label="Tiempo completo" />
-                <FormControlLabel value="practicante" control={<Radio sx={{p:.5}} size='small' />} label="Practicante" />
-                <FormControlLabel value="temporal" control={<Radio sx={{p:.5}} size='small' />} label="Temporal" />
-                <FormControlLabel value="proyecto" control={<Radio sx={{p:.5}} size='small' />} label="Proyecto" />
-                <FormControlLabel value="" control={<Radio sx={{p:.5}} size='small' />} label="Limpiar" />
+                <FormControlLabel value="medio tiempo" control={<Radio sx={{ p: .5 }} size='small' />} label="Medio tiempo" />
+                <FormControlLabel value="tiempo completo" control={<Radio sx={{ p: .5 }} size='small' />} label="Tiempo completo" />
+                <FormControlLabel value="practicante" control={<Radio sx={{ p: .5 }} size='small' />} label="Practicante" />
+                <FormControlLabel value="temporal" control={<Radio sx={{ p: .5 }} size='small' />} label="Temporal" />
+                <FormControlLabel value="proyecto" control={<Radio sx={{ p: .5 }} size='small' />} label="Proyecto" />
+                <FormControlLabel value="limpiar" control={<Radio sx={{ p: .5 }} size='small' />} label="Limpiar" />
             </RadioGroup>
         </li>
         {/* <li>
@@ -56,16 +73,18 @@ const Filters = () => {
         </li> */}
         <li>
             <strong>Horario</strong>
-            <FormGroup
+            <RadioGroup
+            onChange={handleScheduleChange}
                 sx={{
                     '& .MuiSvgIcon-root': { color: '#656565' },
                     color: '#494949', pl: 2, fontSize: '.1em'
                 }}>
-                <FormControlLabel control={<Checkbox sx={{p:.5}} size='small' />} label="nocturno" />
-                <FormControlLabel control={<Checkbox sx={{p:.5}} size='small' />} label="vespertino" />
-                <FormControlLabel control={<Checkbox sx={{p:.5}} size='small' />} label="matutino" />
-                <FormControlLabel control={<Checkbox sx={{p:.5}} size='small' />} label="rotativo" />
-            </FormGroup>
+                <FormControlLabel value="nocturno" control={<Radio sx={{ p: .5 }} size='small' />} label="Nocturno" />
+                <FormControlLabel value="vespertino" control={<Radio sx={{ p: .5 }} size='small' />} label="Vespertino" />
+                <FormControlLabel value="matutino" control={<Radio sx={{ p: .5 }} size='small' />} label="Matutino" />
+                <FormControlLabel value="rotativo" control={<Radio sx={{ p: .5 }} size='small' />} label="Rotativo" />
+                <FormControlLabel value="limpiar" control={<Radio sx={{ p: .5 }} size='small' />} label="Limpiar" />
+            </RadioGroup>
         </li>
     </ul>
 }
