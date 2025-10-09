e9208cd6c25813e9544fe093c13b95229370b00b
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-09 08:07:32 -0700

    fix(schema): update min values and remove optional flag

--- src/Components/NewJobForm/schemas/validation.form.ts
+++ src/Components/NewJobForm/schemas/validation.form.ts
@@ -2,8 +2,8 @@ import {z} from 'zod';
 import { contractTypes, industries, schedules } from '../../../types/job';
 export const jobSchema = z.object({
     "title":z.string().min(5, {message:'El titulo es muy corto'}).max(100, {message:'El titulo es muy largo'}),
-    "short_description":z.string().min(5, {message:'El tiene que ser muy corta'}).max(250, {message:'El descripcion es muy larga'}),
-    "description":z.string().min(5).max(700),
+    "short_description":z.string().min(17, {message:'El tiene que ser muy corta'}).max(250, {message:'El descripcion es muy larga'}),
+    "description":z.string().min(17).max(2500),
     "salary":z.string().min(1, {message:'Este campo es obligatorio'}),
     "benefits": z.array(z.string())
         .min(1, {message:'debe de agregar al menos un benefocio'})
@@ -11,5 +11,5 @@ export const jobSchema = z.object({
     "industry":z.enum(industries, {message:'seleccione una industria'}),
     "schedule":z.enum(schedules),
     "contract_type":z.enum(contractTypes),
-    "published":z.boolean().optional().default(false)
+    "published":z.boolean()
 })
