b62cfbf9aa6d9ea2c87c7cec88f252357e1d17a1
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-12 22:19:52 -0700

    feat: create rich text field component

---
+++ src/Components/RichTextfield/index.tsx
@@ -0,0 +1,46 @@
+import StarterKit from '@tiptap/starter-kit'
+import { MenuButtonBold, MenuButtonBulletedList, MenuButtonItalic, MenuButtonOrderedList, MenuButtonStrikethrough, MenuButtonUnderline, MenuControlsContainer, MenuDivider, MenuSelectHeading, RichTextEditor } from 'mui-tiptap';
+import Underline from '@tiptap/extension-underline';
+
+const RichTextfield:React.FC<{value: string,
+    onChange: (value: string) => void, error?: boolean;  }> = ({ value, onChange, error}) => {
+
+    return <RichTextEditor
+        sx={{
+            border:`.09em solid ${error?'#d32f2f':'#e0e0e0'}`,
+            borderRadius:4,
+            overflow:'hidden',
+            '&:hover':{ borderColor: '#c9c9c9',},
+            '& .MuiTiptap-FieldContainer-notchedOutline': {
+                border:0,
+            // borderColor: '#c9c9c9',
+        },
+        '& .ProseMirror': {
+            padding: '16px',
+            minHeight: '150px',
+            fontSize: '14px',
+        },
+        }}
+        extensions={[StarterKit, Underline]}
+        content={value}
+        onUpdate={({ editor }) => {
+            onChange(editor.getHTML());
+        }}
+        renderControls={() => (
+            <MenuControlsContainer>
+                <MenuSelectHeading />
+                <MenuDivider />
+                <MenuButtonBold />
+                <MenuButtonItalic />
+                <MenuButtonUnderline />
+                <MenuButtonStrikethrough />
+                <MenuDivider />
+                <MenuButtonBulletedList />
+                <MenuButtonOrderedList />
+            </MenuControlsContainer>
+        )}
+    />
+
+}
+
+export default RichTextfield;
