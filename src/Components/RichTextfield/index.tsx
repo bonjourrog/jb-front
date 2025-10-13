7d298b2b90041df44f33e90f8fdbf37901a553f7
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-10-12 22:44:08 -0700

    fix: fix rich text editor, description value is not setting when editing a job

--- src/Components/RichTextfield/index.tsx
+++ src/Components/RichTextfield/index.tsx
@@ -1,25 +1,40 @@
 import StarterKit from '@tiptap/starter-kit'
-import { MenuButtonBold, MenuButtonBulletedList, MenuButtonItalic, MenuButtonOrderedList, MenuButtonStrikethrough, MenuButtonUnderline, MenuControlsContainer, MenuDivider, MenuSelectHeading, RichTextEditor } from 'mui-tiptap';
+import { MenuButtonBold, MenuButtonBulletedList, MenuButtonItalic, MenuButtonOrderedList, MenuButtonStrikethrough, MenuButtonUnderline, MenuControlsContainer, MenuDivider, MenuSelectHeading, RichTextEditor, RichTextEditorRef } from 'mui-tiptap';
 import Underline from '@tiptap/extension-underline';
+import { useEffect, useRef } from 'react';
 
-const RichTextfield:React.FC<{value: string,
-    onChange: (value: string) => void, error?: boolean;  }> = ({ value, onChange, error}) => {
+const RichTextfield: React.FC<{
+    value: string,
+    onChange: (value: string) => void, error?: boolean;
+}> = ({ value, onChange, error }) => {
+    console.log(value);
+    const rteRef = useRef<RichTextEditorRef>(null);
+    useEffect(() => {
+        const editor = rteRef.current?.editor;
+        if (editor && !editor.isDestroyed) {
+            const currentContent = editor.getHTML();
+            if (currentContent !== value) {
+                editor.commands.setContent(value || '');
+            }
+        }
+    }, [value]);
 
     return <RichTextEditor
+        ref={rteRef}
         sx={{
-            border:`.09em solid ${error?'#d32f2f':'#e0e0e0'}`,
-            borderRadius:4,
-            overflow:'hidden',
-            '&:hover':{ borderColor: '#c9c9c9',},
+            border: `.09em solid ${error ? '#d32f2f' : '#e0e0e0'}`,
+            borderRadius: 4,
+            overflow: 'hidden',
+            '&:hover': { borderColor: '#c9c9c9', },
             '& .MuiTiptap-FieldContainer-notchedOutline': {
-                border:0,
-            // borderColor: '#c9c9c9',
-        },
-        '& .ProseMirror': {
-            padding: '16px',
-            minHeight: '150px',
-            fontSize: '14px',
-        },
+                border: 0,
+                // borderColor: '#c9c9c9',
+            },
+            '& .ProseMirror': {
+                padding: '16px',
+                minHeight: '150px',
+                fontSize: '14px',
+            },
         }}
         extensions={[StarterKit, Underline]}
         content={value}
