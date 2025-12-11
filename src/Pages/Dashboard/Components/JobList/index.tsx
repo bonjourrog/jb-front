78305e5eb9b4f3010c92c3587a4082f10b1becff
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-12-11 11:24:04 -0700

    feat: implement JobRow component

--- src/Pages/Dashboard/Components/JobList/index.tsx
+++ src/Pages/Dashboard/Components/JobList/index.tsx
@@ -1,22 +1,21 @@
 import './JobList.css';
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { jwtDecode as jwt_decode } from 'jwt-decode';
 import { useAuthStore } from "../../../../stores/authStore";
 import { Filter } from "../../../../entity/filter";
 import { useJobs } from "../../../../hooks/useJobs";
-import { FormControlLabel, FormGroup, Menu, MenuItem } from '@mui/material';
+import { Menu, MenuItem } from '@mui/material';
 import { MdAdd } from 'react-icons/md';
-import JobListForm from '../../../../Components/NewJobForm';
-import { Delete, Edit, MoreVert } from '@mui/icons-material';
+import { Delete, Edit } from '@mui/icons-material';
 import { Job } from '../../../../entity/job';
 import { useJobStore } from '../../../../stores/jobStore';
-import SwitchComponent from './Components/SwitchComponent';
-import { NewJobData } from '../../../../types/job';
+import NewJobForm from '../../../../Components/NewJobForm';
+import JobRow from './Components/JobRow';
 
 const JobList = () => {
     const { token } = useAuthStore();
     if (!token) return <div>No autorizado</div>;
-    const decoded: any = jwt_decode(token);
+    const decoded: any = useMemo(()=>jwt_decode(token),[token])
     const [showForm, setShowForm] = useState<boolean>(false);
     const [jobSelected, setJobSelected] = useState<Job | null>(null);
     const [menuState, setMenuState] = useState<{ anchorEl: null | HTMLElement, isOpen: boolean, jobId: string }>({
@@ -31,10 +30,8 @@ const JobList = () => {
         schedule: '',
         search: ''
     });
-    const { deleteJob, updateJob } = useJobs(filters);
+    const { deleteJob } = useJobs(filters);
     const jobs = useJobStore(state => state.jobs);
-    console.log('jobs');
-
     const setJobs = useJobStore(state => state.setJobs);
 
     const handleJobDelete = async () => {
@@ -44,25 +41,13 @@ const JobList = () => {
         const newJobs: Job[] = jobs.filter(j => j._id !== _jobSelected._id)
         setJobs(newJobs);
     }
-    const handleToggle = async (event: React.ChangeEvent<HTMLInputElement>, job_id: string) => {
-        const newJobs = jobs.map(job => {
-            if (job._id === job_id) {
-                return { ...job, published: event.target.checked };
-            }
-            return job;
-        });
-        setJobs(newJobs);
-        const job: Job = jobs.filter(j => j._id === job_id)[0];
-        const JobToUpdate: NewJobData = { ...job, published: event.target.checked, salary: `${job.salary}` };
-        await updateJob(JobToUpdate);
-    }
-
-
+    
+    
     return <section className='p-0 md:p-10'>
         {
             showForm ?
                 <div className={`job-list-form-container`}>
-                    <JobListForm setShowForm={setShowForm} jobData={jobSelected} />
+                    <NewJobForm setShowForm={setShowForm} jobData={jobSelected} />
                 </div>
                 : undefined
         }
@@ -79,44 +64,13 @@ const JobList = () => {
                 </thead>
                 <tbody>
                     {jobs.map(job => (
-                        <tr key={job._id}>
-                            <td className='hover:bg-gray-100' onClick={(event) => {
-                                setMenuState({
-                                    anchorEl: event.currentTarget,
-                                    isOpen: true,
-                                    jobId: job._id
-                                });
-                            }}>
-
-                                <div className='flex gap-2 justify-center items-start'>
-                                    <MoreVert className='text-gray-400' />
-                                </div>
-                            </td>
-                            <td className='editable'>
-                                <strong>{job.title}</strong>
-                            </td>
-                            <td className='editable hidden md:table-cell'>
-                                <p className='text-zinc-500'>
-                                    {Math.trunc(job.salary).toLocaleString('en-US', { style: 'currency', currency: 'usd', minimumFractionDigits: 0 })}
-                                </p>
-                            </td>
-                            <td className='editable hidden lg:table-cell'>
-                                <div className='flex gap-2'>
-                                    {job.benefits.map(benefit => (
-                                        <p key={benefit} className='px-2 py-1 text-xs text-emerald-600 bg-emerald-200 rounded-sm'>{benefit}</p>
-                                    ))}
-                                </div>
-                            </td>
-                            <td className='editable w-10'>
-                                <FormGroup>
-                                    <FormControlLabel
-                                        control={<SwitchComponent sx={{ m: 1 }} checked={job.published} onChange={(e) => { handleToggle(e, job._id) }} />}
-                                        label=""
-                                    />
-                                    {job.published}
-                                </FormGroup>
-                            </td>
-                        </tr>
+                        <JobRow job={job} filters={filters} setStateMenu={(event:React.MouseEvent<HTMLTableDataCellElement, MouseEvent>)=>{
+                            setMenuState({
+                                anchorEl: event.currentTarget,
+                                isOpen: true,
+                                jobId: job._id
+                            });
+                        }}/>
                     ))}
                 </tbody>
             </table> : <p className='px-3 py-6 font-extrabold text-3xl text-zinc-700'>Aun no tienes empleos registrados</p>}
