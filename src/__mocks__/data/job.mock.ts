a4a9bcd45d44f30599eaca0254f1874dbe4d61a5
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-12-11 10:55:11 -0700

    feat: create JOB_DATA_MOCK

---
+++ src/__mocks__/data/job.mock.ts
@@ -0,0 +1,23 @@
+import { Job } from "../../entity/job";
+
+export const JOB_DATA_MOCK: Job = {
+    _id: "68de965db1f37fb944857f78",
+    benefits: ["Seguro médico", "Bonos por desempeño"],
+    company_id: "68327b6197f4bfe459b15aa5",
+    company_logo: "https://i.pinimg.com/736x/dd/46/99/dd4699f47c8cd47c6b0181590004cfda.jpg",
+    company_name: "Burger King",
+    company_phone: "6381565384",
+    contract_type: "tiempo completo",
+    created_at: new Date("2025-10-02T15:12:29.107Z"),
+    description: "<p>Se requiere desarrollador frontend con conocimiento en React, JavaScript y experiencia mínima de 2 años. Trabajo en equipo dinámico con proyectos innovadores.</p><p></p><p><strong>Requisitos</strong></p><ul><li><p>React</p></li><li><p>es6</p></li><li><p>sql</p></li><li><p>zustand</p></li><li><p>redux</p></li></ul><p></p><p><strong>Que ofrecemos?</strong></p><ul><li><p>seguro</p></li><li><p>Buen sueldo</p></li><li><p>Vacaciones</p></li></ul><p></p>",
+    industry: "Tecnología",
+    is_formal_job: true,
+    location: { type: "", coordinates: [] },
+    published: false,
+    salary: 35000,
+    schedule: "matutino",
+    short_description: "Se busca desarrollador con experiencia en React",
+    slug: "",
+    title: "desarrollador frontend",
+    updated_at: new Date("2025-10-15T03:43:19.525Z"),
+}
